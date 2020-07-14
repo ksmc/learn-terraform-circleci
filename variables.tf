2c555301c7bd5137e716d5e24dce23151e124dfe
watsonjm <jwatson@ksmconsulting.com>
2020-07-14 13:00:37 -0400

    updated variables

--- variables.tf
+++ variables.tf
@@ -1,4 +1,17 @@
-variable "region" {}
-variable "user" {}
-variable "label" {}
-variable "app" {}
+
+variable "region" {
+  type    = string
+  default = "us-west-2"
+}
+variable "environment" {
+  type    = string
+  default = "test"
+}
+variable "managed" {
+  type = string
+  default = "terraform"
+}
+variable "group" {
+  type = string
+  default = "infra"
+}
