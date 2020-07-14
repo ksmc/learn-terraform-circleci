d515c2b2df24449c0559a9e2ca6084549fa71f70
watsonjm <jwatson@ksmconsulting.com>
2020-07-14 12:57:43 -0400

    Changed to only create VPC resource in us-west-2 (oregon)

--- main.tf
+++ main.tf
@@ -1,11 +1,7 @@
 terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "<YOUR-ORG-NAME>"
+  required_version = ">= 0.12"
+  backend "s3" {
 
-    workspaces {
-      name = "learn-terraform-circleci"
-    }
   }
 }
 
@@ -13,52 +9,26 @@ provider "aws" {
   region = var.region
 }
 
-provider "template" {
-}
-
-resource "aws_iam_user" "circleci" {
-  name = var.user
-  path = "/system/"
-}
-
-resource "aws_iam_access_key" "circleci" {
-  user = aws_iam_user.circleci.name
-}
-
-data "template_file" "circleci_policy" {
-  template = file("circleci_s3_access.tpl.json")
-  vars = {
-    s3_bucket_arn = aws_s3_bucket.portfolio.arn
+locals {
+  environment = var.environment
+  common_tags = {
+    namespace   = var.namespace
+    environment = var.environment
+    managed     = var.managed
+    group       = var.group
   }
 }
 
-resource "local_file" "circle_credentials" {
-  filename = "tmp/circleci_credentials"
-  content  = "${aws_iam_access_key.circleci.id}\n${aws_iam_access_key.circleci.secret}"
+resource "aws_vpc" "testvpc" {
+  cidr_block           = var.cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  enable_classiclink   = false
+  instance_tenancy     = "default"
+
+  tags = merge(local.common_tags, { Name = "${var.environment}-hhs-mainvpc" })
 }
 
-resource "aws_iam_user_policy" "circleci" {
-  name   = "AllowCircleCI"
-  user   = aws_iam_user.circleci.name
-  policy = data.template_file.circleci_policy.rendered
-}
-
-resource "aws_s3_bucket" "portfolio" {
-  tags = {
-    Name = "Portfolio Website Bucket"
-  }
-
-  bucket = "${var.app}.${var.label}"
-  acl    = "public-read"
-
-  website {
-    index_document = "${var.app}.html"
-    error_document = "error.html"
-  }
-  force_destroy = true
-
-}
-
-output "Endpoint" {
-  value = aws_s3_bucket.portfolio.website_endpoint
+output "VPC_ID" {
+  value = aws_vpc.testvpc.id
 }
